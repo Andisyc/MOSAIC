7c9b0761fe73353bdff4cef599cb5237ef5689a0
Andisyc <Andisyc@163.com>
2026-03-28 11:06:54 +0800

    update run_mosaic_gmt.sh

--- run/run_mosaic_gmt.sh
+++ run/run_mosaic_gmt.sh
@@ -19,11 +19,16 @@
 #     --log_project_name GMT_MOSAIC_RL \
 #     --run_name GMT_MOSAIC_GMT
 
+# stage pre: testing run
+HYDRA_FULL_ERROR=1 python scripts/rsl_rl/train.py \
+    --task=General-Tracking-Flat-G1-Wo-State-Estimation-v0-World-Coordinate-Reward \
+    --num_envs=2 \
+    --motion /home/chengyuxuan/MOSAIC/q_npz \
+    --logger wandb \
+    --headless \
+    --device cuda:0
+
 # stage 0: MOSAIC GMT training
-
-# real training: --num_envs=12000 --motion /ssd1/chengyuxuan/AMASS_G1NPZ_Final
-# print testing: --num_envs=2 --motion /home/chengyuxuan/MOSAIC/q_npz
-
 HYDRA_FULL_ERROR=1 nohup bash ~/IsaacLab_mosaic/isaaclab.sh -p ~/MOSAIC/scripts/rsl_rl/train.py \
     --task=General-Tracking-Flat-G1-Wo-State-Estimation-v0-World-Coordinate-Reward \
     --num_envs=12000 \
@@ -33,23 +38,19 @@ HYDRA_FULL_ERROR=1 nohup bash ~/IsaacLab_mosaic/isaaclab.sh -p ~/MOSAIC/scripts/
     --device cuda:0 >~/MOSAIC/train.txt 2>&1 &
 
 # stage 1: FrontRES Supervised training
-
 HYDRA_FULL_ERROR=1 bash ~/IsaacLab_mosaic/isaaclab.sh -p ~/MOSAIC/scripts/rsl_rl/train.py \
     --task=FrontRES-Supervised-Tracking-Flat-G1-v0 \
     --num_envs=12000 \
-    --motion /home/yuxuancheng/MOSAIC/q_npz/ \
-    --headless \
+    --motion /ssd1/chengyuxuan/AMASS_G1NPZ_Final \
     --logger wandb \
     --headless \
     --device cuda:0
 
 # stage 2: FrontRES RL training
-
 HYDRA_FULL_ERROR=1 bash ~/IsaacLab_mosaic/isaaclab.sh -p ~/MOSAIC/scripts/rsl_rl/train.py \
     --task=FrontRES-RLFinetune-Tracking-Flat-G1-v0 \
     --num_envs=12000 \
-    --motion /home/yuxuancheng/MOSAIC/q_npz/ \
-    --headless \
+    --motion /ssd1/chengyuxuan/AMASS_G1NPZ_Final \
     --logger wandb \
     --headless \
     --device cuda:0
