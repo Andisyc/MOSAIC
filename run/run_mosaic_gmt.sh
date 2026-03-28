80c96b4ffb8cdcd8b4bdfc57df5eb70bdf41a7a3
Andisyc <Andisyc@163.com>
2026-03-28 11:00:00 +0800

    update run_mosaic_gmt.sh

--- run/run_mosaic_gmt.sh
+++ run/run_mosaic_gmt.sh
@@ -1,8 +1,4 @@
-# 1st stage
-# MOSAIC GMT training
-
 # Distribute Training
-
 # HYDRA_FULL_ERROR=1 torchrun --standalone --nnodes=1 --nproc_per_node=8 scripts/rsl_rl/train.py \
 #     --task=General-Tracking-Flat-G1-Wo-State-Estimation-v0-World-Coordinate-Reward \
 #     --distributed \
@@ -14,7 +10,6 @@
 #     --run_name GMT_MOSAIC_GMT
 
 # Single GPU Training
-
 # HYDRA_FULL_ERROR=1 python scripts/rsl_rl/train.py \
 #     --task=General-Tracking-Flat-G1-Wo-State-Estimation-v0-World-Coordinate-Reward \
 #     --num_envs=12000 \
@@ -24,7 +19,10 @@
 #     --log_project_name GMT_MOSAIC_RL \
 #     --run_name GMT_MOSAIC_GMT
 
-# Self Trained GMT
+# stage 0: MOSAIC GMT training
+
+# real training: --num_envs=12000 --motion /ssd1/chengyuxuan/AMASS_G1NPZ_Final
+# print testing: --num_envs=2 --motion /home/chengyuxuan/MOSAIC/q_npz
 
 HYDRA_FULL_ERROR=1 nohup bash ~/IsaacLab_mosaic/isaaclab.sh -p ~/MOSAIC/scripts/rsl_rl/train.py \
     --task=General-Tracking-Flat-G1-Wo-State-Estimation-v0-World-Coordinate-Reward \
@@ -34,12 +32,21 @@ HYDRA_FULL_ERROR=1 nohup bash ~/IsaacLab_mosaic/isaaclab.sh -p ~/MOSAIC/scripts/
     --headless \
     --device cuda:0 >~/MOSAIC/train.txt 2>&1 &
 
-# FrontRES test run
-
-# /home/yuxuancheng/MOSAIC/q_npz/01_01_poses.npz
+# stage 1: FrontRES Supervised training
 
 HYDRA_FULL_ERROR=1 bash ~/IsaacLab_mosaic/isaaclab.sh -p ~/MOSAIC/scripts/rsl_rl/train.py \
     --task=FrontRES-Supervised-Tracking-Flat-G1-v0 \
+    --num_envs=12000 \
+    --motion /home/yuxuancheng/MOSAIC/q_npz/ \
+    --headless \
+    --logger wandb \
+    --headless \
+    --device cuda:0
+
+# stage 2: FrontRES RL training
+
+HYDRA_FULL_ERROR=1 bash ~/IsaacLab_mosaic/isaaclab.sh -p ~/MOSAIC/scripts/rsl_rl/train.py \
+    --task=FrontRES-RLFinetune-Tracking-Flat-G1-v0 \
     --num_envs=12000 \
     --motion /home/yuxuancheng/MOSAIC/q_npz/ \
     --headless \
